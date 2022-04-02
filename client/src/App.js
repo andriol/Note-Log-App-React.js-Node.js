fbd01724e71da06548808c47ef2604aefb450288
andriol <keli87aj@hotmail.com>
2022-04-03 00:48:44 +0300

    display notes from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import NoteList from './components/NoteList';
 import Note from './components/Note';
+import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    let notes;
 
     this.state = {
       notes: [],
     };
     this.addHandle = this.addHandle.bind(this);
   }
+  // add note
   addHandle(note) {
-    const id = uuidv4();
-    const newNote = {
-      id,
-      text: note,
-      date: new Date(),
-    };
-    this.setState({
-      notes: [newNote, ...this.state.notes],
-    });
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/notes`, { text: note })
+      .then(({ data }) => {
+        console.log(data.data);
+        this.setState({
+          notes: [data, ...this.state.notes],
+        });
+      });
   }
 
+  // display notes
+  componentDidMount() {
+    axios.get(`${process.env.REACT_APP_API_URL}/notes`).then(({ data }) => {
+      const dataArray = data.sort(function (a, b) {
+        return new Date(b.date) - new Date(a.date);
+      });
+
+      this.setState({
+        notes: dataArray,
+      });
+    });
+  }
   render() {
     console.log(this.state.notes);
     return (
